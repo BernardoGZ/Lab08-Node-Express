5190417a9a6487942f8da45ac5dab99e5c35529d
BernardoG <A00570682@itesm.mx>
2021-04-28 23:24:09 -0500

    Post reservs done, couldnt do it with independent app.js

--- backend/server.js
+++ backend/server.js
@@ -63,10 +63,6 @@ var listaRsv = [];
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newtable = req.body;
-
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
   
     console.log(newtable);
   
